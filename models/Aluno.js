d284a037c462b241120d5cf519bdb90d94e90f98
IvaneideLucio <ivaneidelucio@gmail.com>
2016-12-12 09:57:50 -0200

    First
    Primeiro Repositório

---
+++ models/Aluno.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var AlunoSchema = new mongoose.Schema({
+  nome: String,
+  idade: Number,
+  turma: String,
+  turno: String,
+  matricula: number
+  
+});
+
+module.exports = mongoose.model('Aluno', AlunoSchema);
