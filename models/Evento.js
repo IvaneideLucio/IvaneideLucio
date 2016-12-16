4142416e23139e0b18be2163bd7c53698a85f3f2
IvaneideLucio <ivaneidelucio@gmail.com>
2016-12-16 10:26:41 -0200

    Ajuste evento com convidados e endereço

--- models/Evento.js
+++ models/Evento.js
@@ -1,17 +1,16 @@
 var mongoose = require('mongoose');
 
 var EventoSchema = new mongoose.Schema({
-  nome: String,
-  descricao: String,
-  dataInicio: String,
-  dataFim: String,
-  local: String,
-  tag: String,
-  convidados: String //como definir vários convidados???
-  
-  
- 
-  
+    nome: String,
+    descricao: String,
+    dataInicio: String,
+    dataFim: String,
+    tag: String,
+    local: String,
+    cep: Number,
+    endereco: String,
+    convidados: String //como definir vários convidados???
+
 });
 
 module.exports = mongoose.model('Evento', EventoSchema);
