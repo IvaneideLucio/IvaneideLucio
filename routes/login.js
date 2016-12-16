9e93b75b7a65280f9001a5a6f23efbb9a22afb0b
IvaneideLucio <ivaneidelucio@gmail.com>
2016-12-16 11:23:57 -0200

    Ajuste login

--- routes/login.js
+++ routes/login.js
@@ -4,19 +4,19 @@ var jwt = require('jsonwebtoken');
 var secretKey = "e>@O103o&0ix-sbkzAr8439+jE5p^C";
 var router = express.Router();
 
-var Usuario = require('../models/Usuario.js');
+
 var Evento = require('../models/Evento.js');
 
 router.post('/', function (req, res, next) {
-    Usuario.findOne({login: req.body.username, senha: req.body.password}, 'login')
-            .exec(function (err, usuario) {
+    Evento.findOne({login: req.body.username, senha: req.body.password}, 'login')
+            .exec(function (err, evento) {
                 if (err)
                     res.send(err);
-                if (usuario !== null) {
-                    var token = jwt.sign(usuario, secretKey, {
+                if (evento !== null) {
+                    var token = jwt.sign(evento, secretKey, {
                         expiresIn: "1 day"
                     });
-                    res.json({usuario: usuario, token: token});
+                    res.json({evento: evento, token: token});
                 } else {
                     res.status(400).send('Login/Senha incorretos');
                 }
